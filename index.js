408eba1b1f52074407ac3e9f9be2a2884caf85bc
Sonya Slegers <sonjay08@gmail.com>
2018-07-27 15:09:12 -0500

    sort stack

--- index.js
+++ index.js
@@ -104,20 +104,22 @@ function is_palindrome(s) {
 function matchingParens(exp) {
   const stack = new Stack;
   const countStack = new Stack;
-  // push each element of exp into stack starting with 0 index
-  // if the char is ( push its count to countStack
-  // if the char is ) pop a value out of countStack
+  // push each element of exp into stack starting with first character
   for (let i = 0; i < exp.length; i++) {
+    // if the char is '(' push its count to countStack
     if (exp.charAt(i) === '(') {
       countStack.push(i);
     } 
+    // if the char is ')' and the countsStack isn't empty pop a value out of countStack
     if (exp.charAt(i) === ')' && countStack.top !== null) {
       countStack.pop(i);
     }
+    // if the char is ')' and the countStack is empty print a message about missing paren match
     if (exp.charAt(i) === ')' && countStack.top === null) {
       console.log(`the ')' at location ${i + 1} is missing its match`)
     }
   }
+  //if the countStack is not empty after the loop print a message about missing paren match
   while (countStack.top !== null) {
     console.log(`the '(' at location ${countStack.top.data + 1} is missing its match`);
     countStack.pop();
@@ -125,6 +127,77 @@ function matchingParens(exp) {
 
 }
 
-matchingParens('1+(1*1)+(1+1((');
+// matchingParens('1+(1*1)+(1+1((');
+
+
+
+/////////////////////////// sort stack ///////////////////////////////////
+
+
+function sortStack(stack) {
+  let sorted = new Stack;
+  let start = stack;
+  let hold = new Stack;
+  let largest = null;
+  // loop through start and hold stacks, alternating
+  while (start.top !== null || hold.top !== null) {
+    // if start stack is full loop through it
+    if (start.top !== null) {
+    // loop through stack by moving it from one stack to another
+    while (start.top !== null) {
+      // if top is greater than 'largest' replace 'largest' 
+      if (start.top.data > largest) {
+        // push prev 'largest' to hold stack
+        if (largest !== null) {
+          hold.push(largest)
+        }
+        largest = start.top.data;
+        start.pop();
+      // push top to hold stack
+      } else {
+        hold.push(start.top.data)
+        start.pop();
+      }
+   }
+   // push largest to sorted stack
+  sorted.push(largest);
+  // set largest back to 0;
+  largest = null;
+  } else {
+    while (hold.top !== null) {
+      // if top is greater than 'largest' replace 'largest' 
+      if (hold.top.data > largest) {
+        if (largest !== null) {
+          start.push(largest)
+        }
+        largest = hold.top.data;
+        hold.pop();
+      // push top to start stack
+      } else {
+        start.push(hold.top.data)
+        hold.pop();
+      }
+    }
+    // push largest back to 0;
+   sorted.push(largest);
+  // set largest back to 0;
+  largest = null;
+  }
+  
+}
+
+  // pop out next variable, compare to 'largest and replace or push to holding stack etc.
+  // at the end of the loop put 'largest' into sorted stack
+  console.log(util.inspect(sorted, false, null));
+}
+
+let numStack = new Stack;
+numStack.push(56);
+numStack.push(45);
+numStack.push(20);
+numStack.push(100);
+numStack.push(139);
+
+sortStack(numStack);
 
 
